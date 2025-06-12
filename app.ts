e1f43246a9879a0e71cea9f9e175259071cf1188
jimz@northern-sucre-joggle <mail@jimz.cc>
2025-06-12 18:05:47 +0800

    update jsdoc for all constant alias functions in app.ts

--- app.ts
+++ app.ts
@@ -13,29 +13,94 @@ import {
 } from "./etc/deps.ts";
 //////////////////////////////////////////////////////////////
 
+// --------------------------------------------------
 // manage items
+// --------------------------------------------------
 /**
  * insert an item into an array with specific position
  * ---------
  * @constant
- * @type {function ( input_array: any[], item_index: number, new_item: any ): any[]}
  * @see `/lib/manage-items.ts`
  */
 const insert_item = manage_items.insert_item;
 
 
+/**
+ * update a specific item in an array
+ * ---------
+ * @constant
+ * @see `/lib/manage-items.ts`
+ */
 const update_item = manage_items.update_item;
+
+
+/**
+ * remove an item from an array
+ * ---------
+ * @constant
+ * @see `/lib/manage-items.ts`
+ */
 const remove_item = manage_items.remove_item;
 
+
+// --------------------------------------------------
 // misc tools
+// --------------------------------------------------
+/**
+ * generate an array of integers with a range defined by start and end
+ * ---------
+ * @constant
+ * @see `/lib/misc-tools.ts`
+ */
 const gen_array_by_range = misc_tools.gen_array_by_range;
+
+
+/**
+ * accepts a number array, add them all up and return the sum
+ * ---------
+ * @constant
+ * @see `/lib/misc-tools.ts`
+ */
 const add_all = misc_tools.add_all;
+
+
+/**
+ * search for given strings in an array with filter
+ * ---------
+ * @constant
+ * @see `/lib/misc-tools.ts`
+ */
 const search = misc_tools.search;
 
+// --------------------------------------------------
 // random tools
+// --------------------------------------------------
+/**
+ * get a random index of an array
+ * ---------
+ * @constant
+ * @see `/lib/random-tools.ts`
+ */
 const random_index = random_tools.random_index;
+
+
+/**
+ * draw an item from an array randomly
+ * ---------
+ * @constant
+ * @see `/lib/random-tools.ts`
+ */
 const random_item = random_tools.random_item;
+
+
+/**
+ * insert an item into an array with random position
+ * ---------
+ * @constant
+ * @see `/lib/random-tools.ts`
+ */
 const random_insert = random_tools.random_insert;
+
 
 //////////////////////////////////////////////////////////////
 /**
